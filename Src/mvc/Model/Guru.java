f1931bd08fa0a216c640ed9796ac18f5c3302c7d
NBN0C <Nwidhya1@students.unnes.ac.id>
2025-05-27 12:38:46 +0700

    Guru

---
+++ Src/mvc/Model/Guru.java
@@ -0,0 +1,167 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mvc.Model;
+
+/**
+ *
+ * @author NBN0C
+ */
+public class Guru {
+    
+    private String nip;
+    private String username;
+    private String password;
+    private String nama;
+    private String alamat;
+    private String noTelp;
+    private String email;
+    private String isActive;
+    private String createdAt;
+    private Integer id;
+    /**
+     * @return the nip
+     */
+    public String getNip() {
+        return nip;
+    }
+
+    /**
+     * @param nip the nip to set
+     */
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the noTelp
+     */
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    /**
+     * @param noTelp the noTelp to set
+     */
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the isActive
+     */
+    public String getIsActive() {
+        return isActive;
+    }
+
+    /**
+     * @param isActive the isActive to set
+     */
+    public void setIsActive(String isActive) {
+        this.isActive = isActive;
+    }
+
+    /**
+     * @return the createdAt
+     */
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * @param createdAt the createdAt to set
+     */
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    
+
+
+    
+}
